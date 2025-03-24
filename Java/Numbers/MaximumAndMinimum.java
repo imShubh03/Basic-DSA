548d4cf210b56932bd9025476d8dd07513c17691
SheshNalwar <nalwarshesh@gmail.com>
2025-03-24 14:07:50 +0530

    Min and max digit of number

---
+++ Java/Numbers/MaximumAndMinimum.java
@@ -0,0 +1,24 @@
+public class MaximumAndMinimum {
+
+    public static void maxAndMin(int num) {
+        int rem = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            max = Math.max(max, lastDigit);
+            min = Math.min(min, lastDigit);
+
+            num = num / 10;
+        }
+
+        System.out.println(min + " is minimum");
+        System.out.println(max + " is maximum");
+
+    }
+
+    public static void main(String[] args) {
+        int num = 4726;
+        maxAndMin(num);
+    }
+}
